c279146cb0a944081d726f7e5cd4a8f71cd7fa11
fanimator12 <torok.franciska.leonora@gmail.com>
2021-10-22 12:09:05 +0200

    init lectures db

--- backend/cds/models.py
+++ backend/cds/models.py
@@ -1,3 +1,19 @@
 from django.db import models  # noqa: F401
 
-# Create your models here.
+
+class Lecture(models.Model):
+    lecture_ID = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    corporateAuthor = models.CharField(max_length=100)
+    abstract = models.TextField()
+    series = models.CharField(max_length=50)
+    speaker = models.CharField(max_length=50)
+    speakerDetails = models.CharField(max_length=50)
+    eventDetails = models.CharField(max_length=50)
+    thumbnailPicture = models.TextField()
+    language = models.CharField(max_length=3)
+    subjectCategory = models.CharField(max_length=50)
+    lectureNote = models.DateTimeField()
+    imprint = models.DurationField()
+    license = models.CharField(max_length=30)
