1f2e291cea120a9d104668a20791e764d90b619b
Harris Tzovanakis <me@drjova.com>
2021-11-02 14:48:48 +0100

    change default permission to readonly

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -131,6 +131,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly"
+    ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
