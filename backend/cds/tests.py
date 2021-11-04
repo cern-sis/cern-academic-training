ff295463345086a4f82fa6db2c1aa0a55691c892
fanimator12 <torok.franciska.leonora@gmail.com>
2021-11-04 15:57:04 +0100

    fix tests

--- backend/cds/tests.py
+++ backend/cds/tests.py
@@ -5,8 +5,6 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.test import APITestCase
 
-from .models import Lecture
-
 # Create your tests here.
 
 
