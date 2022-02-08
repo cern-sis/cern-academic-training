4187cb4a25a9b0f9206d85f1664a4b3f39aaef9a
Harris Tzovanakis <me@drjova.com>
2022-02-08 11:13:38 +0100

    add local proxy

--- ui/src/api/api_root.tsx
+++ ui/src/api/api_root.tsx
@@ -1,12 +1,7 @@
 import axios from "axios";
 
-const token = `http://localhost:8000/admin/authtoken/tokenproxy/1`;
-
 const api = axios.create({
-  baseURL: `http://localhost:8000/api/v1/`,
-  timeout: 1000,
-  withCredentials: false,
-  headers: { Authorization: "Bearer " + token },
+  baseURL: `/api/v1/`,
 });
 
 export function getApiRoot() {
