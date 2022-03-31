1f22b8cde1d7da78f9a694d1c33132d5a1ea72bd
fanimator12 <torok.franciska.leonora@gmail.com>
2022-03-31 19:11:56 +0200

    ant component and about us changes

--- ui/src/components/CERN_FOOTER.tsx
+++ ui/src/components/CERN_FOOTER.tsx
@@ -10,6 +10,116 @@ const { Title } = Typography;
 function CERN_FOOTER() {
   return (
     <Footer id="cern-footer">
+      {/* <div className="footer">
+        <Row className="row" justify="center" align="top" gutter={{ xs: 12, sm: 3, md: 3, lg: 3 }}>
+
+          <Col className="footer-first-col" key="footer-first-col" span={6}>
+            <nav
+              role="navigation"
+              aria-labelledby="block-cernclean-footer-menu"
+              id="block-cernclean-footer"
+            >
+              <Title level={2}
+                className="visually-hidden"
+                id="block-cernclean-footer-menu"
+              >
+                Footer menu
+              </Title>
+
+              <Menu className="menu nav">
+                <Menu.Item>
+                  <Typography.Link href="https://home.cern">CERN</Typography.Link>
+                </Menu.Item>
+              </Menu>
+            </nav>
+          </Col>
+
+          <Col className="footer-second-col" key="footer-second-col" span={6} >
+            <Row className="row" gutter={{ xs: 12, sm: 8, md: 8, lg: 8 }}>
+              <div className="mb-4">
+                <nav
+                  role="navigation"
+                  aria-labelledby="block-generalinfo-2-menu"
+                  id="block-generalinfo-2"
+                >
+                  <Title level={2} id="block-generalinfo-2-menu">General info</Title>
+                  <Menu className="menu nav">
+                    <Menu.Item>
+                      <Typography.Link href="https://home.cern/data-privacy-protection-policy">
+                        Privacy policy
+                      </Typography.Link>
+                    </Menu.Item>
+                    <Menu.Item>
+                      <Typography.Link href="https://copyright.web.cern.ch/">
+                        Copyright and terms of use{" "}
+                      </Typography.Link>
+                    </Menu.Item>
+                  </Menu>
+                </nav>
+              </div>
+            </Row>
+          </Col>
+
+          <Col className="footer-third-col" key="footer-third-col" span={6} offset={0}>
+            <Row className="row" gutter={{ xs: 12, sm: 8, md: 8, lg: 8 }}>
+              <div className="mb-4">
+                <nav
+                  role="navigation"
+                  aria-labelledby="block-cernandyou-2-menu"
+                  id="block-cernandyou-2"
+                >
+                  <Title level={2} id="block-cernandyou-2-menu">
+                    Academic Training Committee
+                  </Title>
+
+                  <Menu className="menu nav">
+
+                    <Menu.Item>
+                      <Link to={`/about-us/`}>About Us</Link>
+                    </Menu.Item>
+
+                    <Menu.Item>
+                      <Menu.Item>
+                        <Typography.Link href="https://cds.cern.ch/collection/Academic%20Training%20Lectures?ln=en">
+                          CERN Document Server
+                        </Typography.Link>
+                      </Menu.Item>
+                      <Typography.Link href="https://indico.cern.ch/category/72/">Indico</Typography.Link>
+                    </Menu.Item>
+
+                    <Menu.Item>
+                      <Typography.Link href="mailto:atc-contact@cern.ch" target="_blank">Contact Us</Typography.Link>
+                    </Menu.Item>
+
+                  </Menu>
+                </nav>
+              </div>
+            </Row>
+
+          </Col>
+
+          <Col className="logo" key="logo" span={6}>
+            <Typography.Link
+              href="https://home.cern/"
+              title="CERN"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img
+                src="https://visit.cern/themes/custom/cernclean/logo.svg"
+                alt="CERN"
+              />
+            </Typography.Link>
+          </Col>
+        </Row> 
+
+      </div>
+
+      <div className="copy-wrapper light">
+        <Typography.Link href="https://copyright.web.cern.ch/">Copyright</Typography.Link>
+        &nbsp;&copy; 2022 CERN
+      </div>*/}
+
       <div className="footer">
         <Row className="row" justify="space-between" gutter={12}>
           <Col
