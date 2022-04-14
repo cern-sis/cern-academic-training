fba64d6b9370faa1fa53d78b6a655259b6a2fd1d
fanimator12 <torok.franciska.leonora@gmail.com>
2022-04-14 12:53:28 +0200

    drawer bug fixed + no results in search added

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -19,7 +19,10 @@ import CERN_TOOLBAR from "./components/CERN_TOOLBAR";
 import { PlayCircleOutlined } from "@ant-design/icons";
 import { getApiRoot } from "./api/api_root";
 import SUGGESTION_BOX from "./components/SUGGESTION_BOX";
+<<<<<<< HEAD
 import LOADING_ICON from "./components/LOADING_ICON";
+=======
+>>>>>>> drawer bug fixed + no results in search added
 
 const { Content } = Layout;
 const { Title } = Typography;
