bf33ec67ac75457c432219526c7c9f6071c8a5fd
fanimator12 <torok.franciska.leonora@gmail.com>
2022-05-30 15:46:15 +0200

    connect to indico

--- harvest/harvest/pipelines.py
+++ harvest/harvest/pipelines.py
@@ -4,10 +4,28 @@
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
 
+import requests
+
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter  # noqa: F401
 
 
 class HarvestPipeline:
+    def __indico(self):
+        self.indico_id = self.event_details.split("/")[4]
+        r = requests.get(
+            "https://indico.cern.ch/export/event/{}.json".format(self.indico_id)
+        )
+        res_data = r.json()
+
+        sponsor = res_data["results"][0]["organizer"].split(" / \\ - ")[0]
+        keywords = res_data["results"][0]["keywords"]
+
+        return sponsor, keywords
+
     def process_item(self, item, spider):
+        sponsor, keywords = self.__indico()
+        item["sponsor"] = sponsor
+        item["keywords"] = keywords
+
         return item
