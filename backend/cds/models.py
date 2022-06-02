689b69d939f64c2a3ac211c6a76af6826b55e240
Harris Tzovanakis <me@drjova.com>
2022-06-02 18:22:10 +0200

    fix more

--- backend/cds/models.py
+++ backend/cds/models.py
@@ -6,7 +6,7 @@ from django.db import models  # noqa: F401
 class Lecture(models.Model):
     lecture_id = models.IntegerField(unique=True, db_index=True)
     title = models.CharField(max_length=250)
-    date = models.DateField(blank=True, default="")
+    date = models.DateField()
     corporate_author = models.CharField(max_length=250, blank=True, default="")
     abstract = models.TextField(blank=True, default="")
     series = models.CharField(max_length=250, blank=True, default="")
@@ -16,7 +16,7 @@ class Lecture(models.Model):
     thumbnail_picture = models.TextField(blank=True, default="")
     language = models.CharField(max_length=3, blank=True, default="")
     subject_category = models.CharField(max_length=250, blank=True, default="")
-    lecture_note = models.DateTimeField(blank=True, default="")
+    lecture_note = models.DateTimeField()
     imprint = models.CharField(max_length=250, blank=True, default="")
     license = models.CharField(max_length=250, blank=True, default="")
     sponsor = models.CharField(max_length=250, blank=True, default="")
