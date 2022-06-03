d8360bea4b61f507434eab32f512e290772b4ad1
Harris Tzovanakis <me@drjova.com>
2022-06-03 17:28:48 +0200

    harvest: fix xml path

--- harvest/harvest/spiders/cds_spider.py
+++ harvest/harvest/spiders/cds_spider.py
@@ -56,9 +56,10 @@ class CDSSpider(Spider):
         records = response.selector.xpath("//record")
 
         LOGGER.info("Harvested records", count=len(records))
-        for selector in records:
+
+        for record in records:
             try:
-                yield self.parse_item(selector, original=selector.get())
+                yield self.parse_item(record, original=record.get())
             except Exception as err:
                 LOGGER.error(err)
 
@@ -67,37 +68,37 @@ class CDSSpider(Spider):
 
         record["type"] = []
 
-        record["lecture_id"] = selector.xpath("//controlfield[@tag=001]/text()").get()
+        record["lecture_id"] = selector.xpath(".//controlfield[@tag=001]/text()").get()
 
         record["title"] = selector.xpath(
-            '//datafield[@tag=245]/subfield[@code="a"]/text()'
+            './/datafield[@tag=245]/subfield[@code="a"]/text()'
         ).get()
 
         record["date"] = selector.xpath(
-            '//datafield[@tag=269]/subfield[@code="c"]/text()'
+            './/datafield[@tag=269]/subfield[@code="c"]/text()'
         ).get()
 
         record["corporate_author"] = selector.xpath(
-            '//datafield[@tag=110]/subfield[@code="a"]/text()'
+            './/datafield[@tag=110]/subfield[@code="a"]/text()'
         ).get()
 
         record["abstract"] = selector.xpath(
-            '//datafield[@tag=520]/subfield[@code="a"]/text()'
+            './/datafield[@tag=520]/subfield[@code="a"]/text()'
         ).get()
 
         series_name = selector.xpath(
-            '//datafield[@tag=490]/subfield[@code="a"]/text()'
+            './/datafield[@tag=490]/subfield[@code="a"]/text()'
         ).get()
 
         series_year = selector.xpath(
-            '//datafield[@tag=490]/subfield[@code="v"]/text()'
+            './/datafield[@tag=490]/subfield[@code="v"]/text()'
         ).get()
 
         if series_name and series_year:
             record["series"] = "{} - {}".format(series_name, series_year)
 
         record["speaker"] = selector.xpath(
-            '//datafield[@tag=700]/subfield[@code="a"]/text()'
+            './/datafield[@tag=700]/subfield[@code="a"]/text()'
         ).get()
 
         record["speaker_details"] = (
@@ -106,38 +107,38 @@ class CDSSpider(Spider):
         )
 
         record["event_details"] = selector.xpath(
-            '//datafield[@tag=856][subfield[@code="y"]="Event details"]/subfield[@code="u"]/text()'
+            './/datafield[@tag=856][subfield[@code="y"]="Event details"]/subfield[@code="u"]/text()'
         ).get()
 
         cover_image = selector.xpath(
-            '//datafield[@tag=856][subfield[@code="x"]="jpgthumbnail"]/subfield[@code="u"]/text()'
+            './/datafield[@tag=856][subfield[@code="x"]="jpgthumbnail"]/subfield[@code="u"]/text()'
         ).get()
 
         if not cover_image:
             cover_image = selector.xpath(
-                '//datafield[@tag=856][subfield[@code="x"]="pngthumbnail"]/subfield[@code="u"]/text()'
+                './/datafield[@tag=856][subfield[@code="x"]="pngthumbnail"]/subfield[@code="u"]/text()'
             ).get()
 
         record["thumbnail_picture"] = cover_image
 
         record["language"] = selector.xpath(
-            '//datafield[@tag=041]/subfield[@code="a"]/text()'
+            './/datafield[@tag=041]/subfield[@code="a"]/text()'
         ).get()
 
         record["subject_category"] = selector.xpath(
-            '//datafield[@tag=650]/subfield[@code="a"]/text()'
+            './/datafield[@tag=650]/subfield[@code="a"]/text()'
         ).get()
 
         record["lecture_note"] = selector.xpath(
-            '//datafield[@tag=518]/subfield[@code="d"]/text()'
+            './/datafield[@tag=518]/subfield[@code="d"]/text()'
         ).get()
 
         duration = selector.xpath(
-            '//datafield[@tag=300]/subfield[@code="a"]/text()'
+            './/datafield[@tag=300]/subfield[@code="a"]/text()'
         ).get()
 
         imprint_date = selector.xpath(
-            '//datafield[@tag=269]/subfield[@code="c"]/text()'
+            './/datafield[@tag=269]/subfield[@code="c"]/text()'
         ).get()
 
         if imprint_date and duration:
@@ -147,7 +148,7 @@ class CDSSpider(Spider):
             '//datafield[@tag=542]/subfield[@code="d"]/text()'
         ).get()
         license_year = selector.xpath(
-            '//datafield[@tag=542]/subfield[@code="g"]/text()'
+            './/datafield[@tag=542]/subfield[@code="g"]/text()'
         ).get()
 
         if license_name and license_year:
