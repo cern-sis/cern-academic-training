d7fd770cb2d74a3fc12992cd0654b893b4474a23
Neodia <ricardoxavier.nunesdeoliveira@gmail.com>
2022-06-09 15:03:21 +0200

    Hide implementation details of API Client.

--- ui/src/services/lectures.service.ts
+++ ui/src/services/lectures.service.ts
@@ -2,13 +2,32 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { Lectures } from '../types/lectures';
 import { Lecture } from '../types/lecture';
 
+interface LectureQuery{
+  searchTerm?: string,
+  currentPage?: number,
+  pageSize?: number
+}
 
 export const lecturesApi = createApi({
   reducerPath: 'lecturesApi',
   baseQuery: fetchBaseQuery({ baseUrl: `/api/v1/` }),
   endpoints: (builder) => ({
-    getLectures: builder.query<Lecture[], string>({
-      query: (query: string) => `/search/lectures/${query}`,
+    getLectures: builder.query<Lecture[], LectureQuery>({
+      query: ({searchTerm, currentPage, pageSize}: LectureQuery) => {
+        let queryParams: string[] = []
+        if(searchTerm)
+          queryParams.push(`search=${searchTerm}`)
+        if(currentPage)
+          queryParams.push(`page=${currentPage}`)
+        if(pageSize)
+          queryParams.push(`page_size=${pageSize}`)
+
+        let queryString = ""
+        if(queryParams.length > 0)
+          queryString = "?" + queryParams.join("&")
+
+        return `/search/lectures/${queryString}`
+      },
       transformResponse: (response: Lectures) => response.results,
     }),
   }),
