d3000c0cadb650a272e487f85555dbc472899204
Neodia <ricardoxavier.nunesdeoliveira@gmail.com>
2022-06-10 12:04:34 +0200

    Fix

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,30 +1,75 @@
-import "./App.css";
-import React, { Fragment, useEffect, useState } from "react";
-import { Link as LinkRouter } from "react-router-dom";
-import {
-  Layout,
-  Row,
-  Col,
-  Card,
-  Carousel,
-  Typography,
-  Divider,
-  Spin,
-} from "antd";
-
-import { getPhotos } from "./photos/carousel/photos";
 import { PlayCircleOutlined } from "@ant-design/icons";
-import { AT_HEADER, CERN_FOOTER, CERN_TOOLBAR, LOADING_ICON, SUGGESTION_BOX } from "./features";
+import { Card, Carousel, Col, Divider, Layout, Row, Typography } from "antd";
+import React, { Fragment } from "react";
+import { Link as LinkRouter } from "react-router-dom";
+import "./App.css";
+import {
+  AT_HEADER,
+  CERN_FOOTER,
+  CERN_TOOLBAR,
+  LOADING_ICON,
+  SUGGESTION_BOX,
+} from "./features";
+import { getPhotos } from "./photos/carousel/photos";
 import { useSearchLecturesQuery } from "./services/lectures.service";
-import { Lecture } from "./types/lecture";
 
 const { Content } = Layout;
 const { Title } = Typography;
 
 function App() {
   const photos = getPhotos();
-  const { data, error, isLoading } = useSearchLecturesQuery({searchTerm: undefined, currentPage: undefined, pageSize: 12});
-  error && console.error(error)
+  const { data, error, isLoading } = useSearchLecturesQuery({
+    searchTerm: undefined,
+    currentPage: undefined,
+    pageSize: 12,
+  });
+
+  const content = error ? (
+    <Card>Oh no, there was an error</Card>
+  ) : isLoading ? (
+    <LOADING_ICON />
+  ) : (
+    data?.map((lecture: any) => {
+      return (
+        <Col key={lecture.lecture_id} xs={24} sm={24} md={12} lg={8} xl={6}>
+          <nav>
+            <LinkRouter
+              to={`/lectures/${lecture.lecture_id}`}
+              key={lecture.lecture_id}
+            >
+              <Card
+                hoverable
+                className="video-card"
+                cover={<img alt="thumbnail" src={lecture.thumbnail_picture} />}
+              >
+                <div className="play">
+                  <PlayCircleOutlined />
+                </div>
+
+                <div className="duration">
+                  <Title level={5}>{lecture.imprint.split(" - ")[1]}</Title>
+                </div>
+
+                <Card.Grid className="card-content" hoverable={false}>
+                  <div className="video-content">
+                    <Title level={4}>{lecture.speaker}</Title>
+                  </div>
+                  <div className="video-content">
+                    <Title level={2}>{lecture.title}</Title>
+                  </div>
+                  <div className="video-content">
+                    <Title level={3}>
+                      Posted on <strong>{lecture.date}</strong>
+                    </Title>
+                  </div>
+                </Card.Grid>
+              </Card>
+            </LinkRouter>
+          </nav>
+        </Col>
+      );
+    })
+  );
 
   window.scrollTo(0, 0);
 
@@ -76,67 +121,7 @@ function App() {
             </div>
             .{" "}
             <Row justify="center" gutter={[16, 42]}>
-              {isLoading ? (
-                <LOADING_ICON />
-              ) : (
-                data?.map((lecture: any) => {
-                  return (
-                    <Col
-                      key={lecture.lecture_id}
-                      xs={24}
-                      sm={24}
-                      md={12}
-                      lg={8}
-                      xl={6}
-                    >
-                      <nav>
-                        <LinkRouter
-                          to={`/lectures/${lecture.lecture_id}`}
-                          key={lecture.lecture_id}
-                        >
-                          <Card
-                            hoverable
-                            className="video-card"
-                            cover={
-                              <img
-                                alt="thumbnail"
-                                src={lecture.thumbnail_picture}
-                              />
-                            }
-                          >
-                            <div className="play">
-                              <PlayCircleOutlined />
-                            </div>
-
-                            <div className="duration">
-                              <Title level={5}>
-                                {lecture.imprint.split(" - ")[1]}
-                              </Title>
-                            </div>
-
-                            <Card.Grid
-                              className="card-content"
-                              hoverable={false}
-                            >
-                              <div className="video-content">
-                                <Title level={4}>{lecture.speaker}</Title>
-                              </div>
-                              <div className="video-content">
-                                <Title level={2}>{lecture.title}</Title>
-                              </div>
-                              <div className="video-content">
-                                <Title level={3}>
-                                  Posted on <strong>{lecture.date}</strong>
-                                </Title>
-                              </div>
-                            </Card.Grid>
-                          </Card>
-                        </LinkRouter>
-                      </nav>
-                    </Col>
-                  );
-                })
-              )}
+              {content}
             </Row>
           </div>
         </Fragment>
